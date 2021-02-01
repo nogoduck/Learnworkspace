b78b56d770484852497e3c98f788ec69115087a3
ad <nogo22@naver.com>
2021-02-02 03:36:10 +0900

    movie save1

--- #project1/client/src/App.js
+++ #project1/client/src/App.js
@@ -1,5 +1,39 @@
+const items = [
+  {
+    name: "OvO",
+    id: "22",
+  },
+  {
+    name: "^[OO]^",
+    id: "GG",
+  },
+  {
+    name: "NAS HDD",
+    id: "ssd",
+  },
+  {
+    name: "Y_Y",
+    id: "1004",
+  },
+];
+
+function Food({ name, id }) {
+  return (
+    <div>
+      <h3>I LOVE {name}</h3>
+      <h4>{id}</h4>
+    </div>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {items.map((a) => (
+        <Food key={a.id} name={a.name} id={a.id} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
