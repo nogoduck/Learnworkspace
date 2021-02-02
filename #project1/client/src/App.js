d46836686ca0cc3f7c7e9280aeed8ed6798e7d12
ad <nogo22@naver.com>
2021-02-03 05:16:39 +0900

    learning comp lifecycle

--- #project1/client/src/App.js
+++ #project1/client/src/App.js
@@ -10,7 +10,20 @@ class App extends React.Component {
   minus = () => {
     this.setState((current) => ({ cnt: current.cnt - 1 }));
   };
+
+  componentDidMount() {
+    console.log("Component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillMount() {
+    //컴포넌트가 떠날 때 호출됌 ex)페이지 이동
+    console.log("Goodbye, cruel world");
+  }
+
   render() {
+    console.log("I`m rendering");
     return (
       <div>
         <h1>The number is : {this.state.cnt}</h1>
