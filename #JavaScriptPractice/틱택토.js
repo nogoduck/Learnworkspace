72f9b056359102746ad57852f8fab48d30d01d48
ad <nogo22@naver.com>
2021-02-22 05:22:44 +0900

    중간저장1 / 5-3

--- #JavaScriptPractice/틱택토.js
+++ #JavaScriptPractice/틱택토.js
@@ -1,10 +1,19 @@
 let body = document.body;
 let table = document.createElement("table");
+let line = [];
+let arr = [];
 
+const func = (e) => {
+  console.log(e.target);
+};
 for (let i = 0; i < 3; i++) {
   var tr = document.createElement("tr");
+  line.push(tr);
+  arr.push([]);
   for (let j = 0; j < 3; j++) {
     let td = document.createElement("td");
+    td.addEventListener("click", func);
+    arr[i].push(td);
     tr.appendChild(td);
   }
 
@@ -12,9 +21,3 @@ for (let i = 0; i < 3; i++) {
 }
 
 body.appendChild(table);
-
-let arr = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
