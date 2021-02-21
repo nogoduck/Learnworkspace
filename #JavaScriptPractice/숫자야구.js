26fc0152390d93a1f1e5294a0dc7f334b064c11a
ad <nogo22@naver.com>
2021-02-22 03:54:33 +0900

    숫자야구 리팩토링

--- #JavaScriptPractice/숫자야구.js
+++ #JavaScriptPractice/숫자야구.js
@@ -23,26 +23,32 @@ form.append(inpButton);
 let answer = document.createElement("div");
 document.body.append(answer);
 
-let numlist = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let numarr = [];
+let numlist;
+let numarr;
 
-for (let i = 0; i < 4; i++) {
-  //   console.log(rdindex);
-  // pop(마지막 배열부터 하나씩 빼낸다), shift(앞에서부터 빼낸다)
-  // push(앞에서부터 하나씩 넣어준다), unshift(뒤에서부터 넣어준다)
-  // splice(특정 인덱스에서 값을 빼준다) => splice 특성상 배열 상태로 뽑히기떄문에 인덱스를 추가로 지정해서 값만 뽑아줘야한다 [0]
-  // ex )splice(4, 3) 4번쨰 인덱스로부터 뒤로 3가지 숫자를 뽑는다
+function randomNumber() {
+  numlist = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  numarr = [];
+  for (let i = 0; i < 4; i++) {
+    //   console.log(rdindex);
+    // pop(마지막 배열부터 하나씩 빼낸다), shift(앞에서부터 빼낸다)
+    // push(앞에서부터 하나씩 넣어준다), unshift(뒤에서부터 넣어준다)
+    // splice(특정 인덱스에서 값을 빼준다) => splice 특성상 배열 상태로 뽑히기떄문에 인덱스를 추가로 지정해서 값만 뽑아줘야한다 [0]
+    // ex )splice(4, 3) 4번쨰 인덱스로부터 뒤로 3가지 숫자를 뽑는다
 
-  //.length 또는 -i 사용
-  //   let k = Math.floor(Math.random() * numlist.length);
-  let k = Math.floor(Math.random() * (9 - i));
-  // console.log(k);
-  let tmp = numlist.splice(k, 1)[0];
+    //.length 또는 -i 사용
+    //   let k = Math.floor(Math.random() * numlist.length);
+    let k = Math.floor(Math.random() * (9 - i));
+    // console.log(k);
+    let tmp = numlist.splice(k, 1)[0];
 
-  numarr.push(tmp);
-  // console.log(tmp);
-  // console.log(numarr);
+    numarr.push(tmp);
+    // console.log(tmp);
+    // console.log(numarr);
+  }
 }
+
+randomNumber();
 
 // 배열 합치기 예시 (배열 => 문자)
 // String(a[0]) + String(a[1]) + String(a[2]) + String(a[3]);
@@ -57,44 +63,38 @@ form.addEventListener("submit", (e) => {
   let val = inpArea.value;
   console.log(val, numarr, val === numarr.join(""));
 
-  if (Number(val) === Number(numarr.join(""))) {
-    problem.textContent = "Home run ⚾";
-    inpArea.value = "";
-    inpArea.focus();
-    cnt = 0;
-    answer.textContent = "";
-    numlist = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    numarr = [];
-    for (let i = 0; i < 4; i++) {
-      let k = Math.floor(Math.random() * (9 - i));
-      let tmp = numlist.splice(k, 1)[0];
-      numarr.push(tmp);
-    }
-  } else {
-    cnt++;
-    // a.indexOf(v)
-    // => a배열의 v의 값이 어느 인덱스에 있는지 찾아준다 값이없으면 -1 반환
-    let valarr = val.split("");
-    let strike = 0;
-    let ball = 0;
-    console.log(numarr, valarr);
-    //JavaScript는 기본적으로 입력한값을 배열로 바꿔줄때도 String 상태로 들어간다
-    //그리하여 Number() 를 사용하여 값이 호출될때마다 바꿔줘야한다
-    // 또는 입력할때 숫자로 넣어도 될것같은데 방법을 아직 찾지 못했다
-
-    for (let i = 0; i < 4; i++) {
-      if (numarr[i] === Number(valarr[i])) {
-        strike++;
-        console.log(strike);
-      } else if (numarr.indexOf(Number(valarr[i])) > -1) {
-        ball++;
-        console.log(ball);
-      }
-    }
-    problem.textContent = strike + "strike, " + ball + "ball";
-  }
   if (cnt < 10) {
     answer.textContent = "Number of remaining: " + (10 - cnt);
+    if (Number(val) === Number(numarr.join(""))) {
+      problem.textContent = "Home run ⚾";
+      inpArea.value = "";
+      inpArea.focus();
+      cnt = 0;
+      answer.textContent = "";
+      randomNumber();
+    } else {
+      cnt++;
+      // a.indexOf(v)
+      // => a배열의 v의 값이 어느 인덱스에 있는지 찾아준다 값이없으면 -1 반환
+      let valarr = val.split("");
+      let strike = 0;
+      let ball = 0;
+      console.log(numarr, valarr);
+      //JavaScript는 기본적으로 입력한값을 배열로 바꿔줄때도 String 상태로 들어간다
+      //그리하여 Number() 를 사용하여 값이 호출될때마다 바꿔줘야한다
+      // 또는 입력할때 숫자로 넣어도 될것같은데 방법을 아직 찾지 못했다
+
+      for (let i = 0; i < 4; i++) {
+        if (numarr[i] === Number(valarr[i])) {
+          strike++;
+          // console.log(strike);
+        } else if (numarr.indexOf(Number(valarr[i])) > -1) {
+          ball++;
+          // console.log(ball);
+        }
+      }
+      problem.textContent = strike + "strike, " + ball + "ball";
+    }
   } else {
     inpArea.focus();
     inpArea.value = "";
@@ -103,12 +103,6 @@ form.addEventListener("submit", (e) => {
       numarr.join("") +
       "<br/>Enter a try again ↩";
     cnt = 0;
-    numlist = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    numarr = [];
-    for (let i = 0; i < 4; i++) {
-      let k = Math.floor(Math.random() * (9 - i));
-      let tmp = numlist.splice(k, 1)[0];
-      numarr.push(tmp);
-    }
+    randomNumber();
   }
 });
