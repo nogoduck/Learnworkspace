1a0c34c951f8281c49bb8a6a9958e71220e0c42f
adx <nogo22@naver.com>
2021-02-22 13:10:27 +0900

    클릭시 인덱스 찾는 이벤트 구현 복습 매우 필요함

--- #JavaScriptPractice/틱택토.js
+++ #JavaScriptPractice/틱택토.js
@@ -3,9 +3,18 @@ let table = document.createElement("table");
 let line = [];
 let arr = [];
 
+//e.target 클릭된 태그
+//e.target.parentNode 클릭된 태그의 부모 태그
+//e.target.children 클릭된 태그의 자식 태그
 const func = (e) => {
-  console.log(e.target);
+  console.log(e.target); //칸 (열) => col(세로)
+  console.log(e.target.parentNode); //줄 (행) => row(가로)
+
+  let row = line.indexOf(e.target.parentNode);
+  let col = arr[row].indexOf(e.target);
+  console.log("행: ", row, "열: ", col);
 };
+//append와 appendChild의 차이점을 아직 모르겠다.
 for (let i = 0; i < 3; i++) {
   var tr = document.createElement("tr");
   line.push(tr);
