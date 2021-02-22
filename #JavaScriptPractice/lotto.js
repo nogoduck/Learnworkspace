e7e23998c5f445989350957760385ff3bf6bd798
ad <nogo22@naver.com>
2021-02-23 01:41:43 +0900

    lotto map 학습

--- #JavaScriptPractice/lotto.js
+++ #JavaScriptPractice/lotto.js
@@ -5,12 +5,25 @@
 //확인방법 :
 //numlist.forEach(function(a){console.log(a)});
 
-var numlist = Array(45);
-numlist = numlist.fill();
+//(1) 생략가능
+// var numlist = Array(45);
+// numlist = numlist.fill();
+
 // console.log(numlist);
 
 //forEach(function(a, b)) => a요소는 값이고 b는 인덱스를 가져온다
-numlist.forEach((val, idx) => {
-  numlist[idx] = idx + 1;
-});
+// numlist.forEach((val, idx) => {
+//   numlist[idx] = idx + 1;
+// });
+// console.log(numlist);
+
+//mapping(맵) :
+//[undefined, undefined, undefined]
+//[1, 2, 3] => 기존의 정보들을 1 : 1 로 매치하여 값을 변경
+//새로운 배열을 가져오기 떄문에 변수도 새로 만들어줘야 한다
+let numlist = Array(45)
+  .fill()
+  .map((val, i) => {
+    return i + 1;
+  });
 console.log(numlist);
