85e3d12a596398e61cb8993a6aa4384a1d26a4ba
ad <nogo22@naver.com>
2021-02-25 03:04:40 +0900

    클릭함수 + 화살표함수 재 학습필요

--- #JavaScriptPractice/RockPaperScissors/RockPaperScissors.js
+++ #JavaScriptPractice/RockPaperScissors/RockPaperScissors.js
@@ -10,8 +10,20 @@ setInterval(() => {
   } else {
     left = 0;
   }
+
   document.querySelector("#computer").style.background =
     "url(https://en.pimg.jp/023/182/267/1/23182267.jpg) " + left + "px 0";
 }, 100);
 
-document.querySelector("#rock").addEventListener("click", () => {});
+//   querySelector는 찾는 클래스가 여러개가 있어도
+// 처음에 선택한 하나만 선택한다
+// 이를 해결하려면 querySelectorAll을 사용하면 된다
+
+document.querySelectorAll(".btn").forEach((btn) => {
+  console.log(btn);
+  //function을 화살표 함수로 사용하면 작동하지 않았다 (undefined)
+  // 화살표 함수의 작동원리를 다시 학습할 필요성이 느껴진다
+  btn.addEventListener("click", function () {
+    console.log(this.textContent);
+  });
+});
