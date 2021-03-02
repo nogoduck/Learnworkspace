1355d9f8001cdf120b51b873a4fd64443104ba94
ad <nogo22@naver.com>
2021-03-03 04:31:15 +0900

    2048 행열 세팅

---
+++ #JavaScriptPractice/2048/2048.js
@@ -0,0 +1,22 @@
+const table = document.querySelector("#table");
+let data = [];
+function Default() {
+  const fragment = document.createDocumentFragment();
+  [1, 2, 3, 4].forEach((y) => {
+    let yData = [];
+    yData.push(y);
+    console.log("y, yData: ", y, yData);
+    let tr = document.createElement("tr");
+    [1, 2, 3, 4].forEach(() => {
+      let xData = [];
+      xData.push(0);
+      console.log("xD: ", xData);
+      let td = document.createElement("td");
+      tr.appendChild(td);
+    });
+  });
+  console.log(fragment);
+  table.appendChild(fragment);
+}
+
+Default();
