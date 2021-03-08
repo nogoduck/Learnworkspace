1e1b761526baa7df0531fe833597b9d2ce1bd6a4
ad <nogo22@naver.com>
2021-03-09 08:12:55 +0900

    카드 색 부여 / 클릭방지

--- #JavaScriptPractice/MatchingCards/MatchingCards.js
+++ #JavaScriptPractice/MatchingCards/MatchingCards.js
@@ -15,6 +15,7 @@ const cardColorList = [
   "pink",
 ];
 
+let isClick = true;
 let mixedColor = [];
 for (let i = 0; 0 < cardColorList.length; i++) {
   console.log(i);
@@ -23,8 +24,9 @@ for (let i = 0; 0 < cardColorList.length; i++) {
   );
   console.log(mixedColor);
 }
-
+//카드 생성
 function setCard(row, col) {
+  isClick = false;
   for (let i = 0; i < row * col; i++) {
     const card = document.createElement("div");
     card.className = "card";
@@ -40,11 +42,29 @@ function setCard(row, col) {
     card.appendChild(cardInner);
     (function (c) {
       card.addEventListener("click", () => {
-        c.classList.toggle("flipped");
+        //카드 세팅이 완료되기 전에 클릭을 금지하기 위한 조건문
+        //작동원리를 아직 이해못했다
+        if (isClick) {
+          c.classList.toggle("flipped");
+        }
       });
     })(card);
     document.body.appendChild(card);
   }
+
+  document.querySelectorAll(".card").forEach((card, i) => {
+    //0.1초 간격으로 순차적으로 카드의 색깔을 공개한다
+    setTimeout(() => {
+      card.classList.add("flipped");
+    }, 1000 + 100 * i);
+    //5초 후 카드 뒤집기
+    setTimeout(() => {
+      document.querySelectorAll(".card").forEach((card) => {
+        card.classList.remove("flipped");
+      });
+      isClick = true;
+    }, 5000);
+  });
 }
 
 setCard(row, col);
