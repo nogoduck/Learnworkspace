91d5d1ad45d5389931c620e7a4353e8c69024650
ad <nogo22@naver.com>
2021-03-10 08:19:23 +0900

    카드 짝 맞추기 구현

--- #JavaScriptPractice/MatchingCards/MatchingCards.js
+++ #JavaScriptPractice/MatchingCards/MatchingCards.js
@@ -16,13 +16,15 @@ const cardColorList = [
 ];
 
 let isClick = true;
+let clickedCard = [];
+let completedCard = [];
 let mixedColor = [];
 for (let i = 0; 0 < cardColorList.length; i++) {
-  console.log(i);
+  // console.log(i);
   mixedColor = mixedColor.concat(
     cardColorList.splice(Math.floor(Math.random() * cardColorList.length), 1)
   );
-  console.log(mixedColor);
+  // console.log(mixedColor);
 }
 //카드 생성
 function setCard(row, col) {
@@ -40,12 +42,40 @@ function setCard(row, col) {
     cardInner.appendChild(cardFront);
     cardInner.appendChild(cardBack);
     card.appendChild(cardInner);
+    //클로저를 해결하기 위해 c 매게변수를 받음
     (function (c) {
       card.addEventListener("click", () => {
         //카드 세팅이 완료되기 전에 클릭을 금지하기 위한 조건문
         //작동원리를 아직 이해못했다
-        if (isClick) {
+        // includes : 배열속에 값의 여부를 확인함
+        if (isClick && !completedCard.includes(c)) {
           c.classList.toggle("flipped");
+          clickedCard.push(c);
+          if (clickedCard.length === 2) {
+            if (
+              clickedCard[0].querySelector(".card-back").style
+                .backgroundColor ===
+              clickedCard[1].querySelector(".card-back").style.backgroundColor
+            ) {
+              console.log("카드가 일치함");
+              console.log("카드 목록: ", clickedCard);
+              completedCard.push(clickedCard[0]);
+              completedCard.push(clickedCard[1]);
+              clickedCard = [];
+              console.log("초기화 후 카드 목록: ", clickedCard);
+            } else {
+              console.log("카드가 일치하지 않음");
+              console.log(clickedCard);
+              //카드 두개의 색깔이 일치하지 않을 때 + 클릭방지
+              isClick = false;
+              setTimeout(() => {
+                clickedCard[0].classList.remove("flipped");
+                clickedCard[1].classList.remove("flipped");
+                isClick = true;
+                clickedCard = [];
+              }, 1000);
+            }
+          }
         }
       });
     })(card);
@@ -63,7 +93,7 @@ function setCard(row, col) {
         card.classList.remove("flipped");
       });
       isClick = true;
-    }, 5000);
+    }, 3000);
   });
 }
 
