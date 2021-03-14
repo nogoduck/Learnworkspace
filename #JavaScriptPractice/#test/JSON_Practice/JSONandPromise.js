17f5806bca0da8fc7ce7a97e9f487e14661f374d
ad <nogo22@naver.com>
2021-03-14 23:16:39 +0900

    json 데이터 js로 가져오기 실패코드

--- #JavaScriptPractice/#test/JSON_Practice/JSONandPromise.js
+++ #JavaScriptPractice/#test/JSON_Practice/JSONandPromise.js
@@ -43,5 +43,21 @@ json = JSON.stringify(Yeti_SB6_info, (key, value) => {
 });
 console.log(json);
 
+// ##########_##########_##########_##########_##########
 // 2. JSON to Object
-// parse(json)
+// parse(json) : JSON => 객체(Object) 변환
+//json 다시 초기화
+json = JSON.stringify(Yeti_SB6_info);
+
+const obj = JSON.parse(json);
+//객체를 JSON으로 변환할 때 있었던 함수는 포함되지 않은 상태로 변환이 되었고
+// 그 JSON 데이터를 다시 객체로 변환하였기 떄문에 함수는 존재하지 않는다
+console.log(obj);
+
+//추가학습 [JSON파일 읽어오기] 이렇게 쓰는게 아니고
+// 외부 자료를 살펴보니 함수를 따로 작성해서 그걸로 불러오는것으로 보인다 또는 JQuery를 사용했다
+//잘못된 코드예시
+// const Yeti_SB6_SpecSheet_JSON = require("./index.json");
+// const Yeti_SB6_SpecSheet_Object = json.parse(Yeti_SB6_SpecSheet_JSON);
+// console.log("JSON Data : ", Yeti_SB6_SpecSheet_JSON);
+// console.log("Object Data : ", Yeti_SB6_SpecSheet_Object);
