467129b43e515de358c13c37de26d98e3e2e98e0
ad <nogo22@naver.com>
2021-03-18 07:03:55 +0900

    add footer_button func

--- exampleStructor/index.js
+++ exampleStructor/index.js
@@ -1,10 +1,13 @@
-const main = document.querySelector("#header .navbar h3");
-const mainImg = document.querySelector("#header .navbar img");
-
-const item1 = document.querySelector("#header .navbar .navbar__menu .item1");
-const item2 = document.querySelector("#header .navbar .navbar__menu .item2");
-const item3 = document.querySelector("#header .navbar .navbar__menu .item3");
-const item4 = document.querySelector("#header .navbar .navbar__menu .item4");
+const main = document.querySelector("#header .navbar h3"),
+  mainImg = document.querySelector("#header .navbar img"),
+  item1 = document.querySelector("#header .navbar .navbar__menu .item1"),
+  item2 = document.querySelector("#header .navbar .navbar__menu .item2"),
+  item3 = document.querySelector("#header .navbar .navbar__menu .item3"),
+  item4 = document.querySelector("#header .navbar .navbar__menu .item4"),
+  footer_favorite = document.querySelector("#footer .footer__items .item1 img"),
+  footer_restart = document.querySelector("#footer .footer__items .item2 img"),
+  footer_close = document.querySelector("#footer .footer__items .item3 img");
+console.log(footer_favorite);
 
 main.addEventListener("click", () => {
   console.log("main");
@@ -30,4 +33,16 @@ item3.addEventListener("click", () => {
 item4.addEventListener("click", () => {
   console.log("item4");
   scrollTo(0, 5153);
+});
+
+footer_favorite.addEventListener("click", () => {
+  console.log("favorite");
+});
+footer_restart.addEventListener("click", () => {
+  location.reload();
+  console.log("restart");
+});
+footer_close.addEventListener("click", () => {
+  window.close();
+  console.log("close");
 });
