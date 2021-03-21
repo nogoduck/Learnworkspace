597fb4cf4bc6440aaf5ef4eece1d0d0cf9b703d6
ad <nogo22@naver.com>
2021-03-22 00:39:19 +0900

    210322save1

---
+++ MyProject/index.js
@@ -0,0 +1,19 @@
+const header_notice = document.querySelector(".notice"),
+  header_nav = document.querySelector(".navagation");
+
+// document.addEventListener("click", () => {
+//   console.log("^0^");
+//   header_notice.style.display = "static";
+//   //   header_nav.style.position = "fixed";
+// });
+
+// document.addEventListener("scroll", () => {
+//   console.log(scrollY);
+//   console.log("HI");
+
+//   if (scrollY > 30) {
+//     header_notice.style.display = "none";
+//   } else {
+//     header_notice.style.display = "static";
+//   }
+// });
