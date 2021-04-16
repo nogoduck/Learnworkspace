9e17e3f0e7958b731d2907cc563808342fc1291a
adx <nogo22@naver.com>
2021-04-16 21:04:29 +0900

    s

--- searchBook/app.js
+++ searchBook/app.js
@@ -1,6 +1,9 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
+console.log(path.join(__dirname, "public"));
+app.use(express.static(path.join(__dirname, "/")));
 app.set("port", process.env.PORT || 8004);
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
@@ -10,6 +13,9 @@ app.get("/", (req, res) => {
   res.render("index.html");
 });
 
+app.get("/search", (req, res) => {
+  res.render("search.html");
+});
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "Port Connected...");
 });
