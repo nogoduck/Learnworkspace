5893d991df4a270d4263c10499a0dac25a6fc92c
ad <nogo22@naver.com>
2021-04-18 02:47:19 +0900

    라우터 분리 실패

--- searchBook/app.js
+++ searchBook/app.js
@@ -1,21 +1,33 @@
 const path = require("path");
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
+const router = express.Router();
+
+const indexRouter = require("./routes/index");
+const searchRouter = require("./routes/search");
 
 console.log(path.join(__dirname, "public"));
+app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "/")));
 app.set("port", process.env.PORT || 8004);
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
-app.get("/", (req, res) => {
-  res.render("index.html");
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "ddd2" });
 });
 
-app.get("/search", (req, res) => {
-  res.render("search.html");
-});
+// app.use("/", indexRouter);
+// app.use("/search", searchRouter);
+// app.get("/", (req, res) => {
+//   res.render("index.html");
+// });
+
+// app.get("/search", (req, res) => {
+//   res.render("search.html");
+// });
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "Port Connected...");
 });
--- searchBook/routes/index.js
+++ searchBook/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("index", { searchList: "ddd" });
+  res.render("index", { title: "ddd2" });
 });
 
 module.exports = router;
