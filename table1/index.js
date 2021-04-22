9a9595b9440174d29c65b2c375a69f4843b5cd06
adx <nogo22@naver.com>
2021-04-23 05:16:22 +0900

    로그아웃 추가

--- table1/index.js
+++ table1/index.js
@@ -80,6 +80,14 @@ app.get("/api/users", auth, (req, res) => {
   });
 });
 
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
+});
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
