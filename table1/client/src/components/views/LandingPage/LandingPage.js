d9ff9104eabe11571b9a2971d0e1ff94745daf23
adx <nogo22@naver.com>
2021-04-24 11:34:15 +0900

    setup proxy

--- table1/client/src/components/views/LandingPage/LandingPage.js
+++ table1/client/src/components/views/LandingPage/LandingPage.js
@@ -2,9 +2,7 @@ import React, { useEffect } from "react";
 import axios from "axios";
 function LandingPage() {
   useEffect(() => {
-    axios
-      .get("http://localhost:8004/api/hello")
-      .then((response) => console.log(response.data));
+    axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
 
   return <div>LandingPage</div>;
