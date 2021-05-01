ba046d33406a2d6355d71d0456c084db647b5224
adu <nogo22@naver.com>
2021-05-02 04:42:00 +0900

    use redux

--- table1/client/src/index.js
+++ table1/client/src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import PromiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
 
+const createStoreWithMiddleware = applyMiddleware(
+  PromiseMiddleware,
+  ReduxThunk
+)(createStore);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
