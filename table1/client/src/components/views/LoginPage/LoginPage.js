bef97afc56c2a96c6237fbe597964775b08e7416
adu <nogo22@naver.com>
2021-05-02 05:18:53 +0900

    s

--- table1/client/src/components/views/LoginPage/LoginPage.js
+++ table1/client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <form>
+        <input />
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
