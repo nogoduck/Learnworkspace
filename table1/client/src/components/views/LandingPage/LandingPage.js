6fa8d3274a815d160c51174bd694aef8f3c6f3ff
adx <nogo22@naver.com>
2021-05-02 12:52:30 +0900

    apply css login, landing page

--- table1/client/src/components/views/LandingPage/LandingPage.js
+++ table1/client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,22 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import "./LandingPage.css";
 function LandingPage() {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
 
-  return <div>LandingPage</div>;
+  return (
+    <div className="main">
+      <h3>환영합니다</h3>
+      <p>
+        <a href="/login">로그인</a>을 하시겠습니까?
+      </p>
+      <p>
+        계정이 없으시다면 <a href="/register">회원가입</a>도 가능합니다
+      </p>
+    </div>
+  );
 }
 
 export default LandingPage;
--- table1/client/src/components/views/LoginPage/LoginPage.js
+++ table1/client/src/components/views/LoginPage/LoginPage.js
@@ -1,10 +1,34 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./LoginPage.css";
 function LoginPage() {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const onPasswordHandler = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("EMAIL: ", Email);
+    console.log("PASSWORD: ", Password);
+  };
+
   return (
-    <div>
-      <form>
-        <input />
+    <div className="main">
+      <h2>Login</h2>
+
+      <form className="loginForm" onSubmit={onSubmitHandler}>
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <br />
+        <button>확인</button>
       </form>
     </div>
   );
