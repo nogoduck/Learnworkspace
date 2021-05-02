2c0214c193ad7bd4be011eae37ffb1269b8de9f1
adx <nogo22@naver.com>
2021-05-02 13:07:13 +0900

    login without redux

--- table1/client/src/components/views/LandingPage/LandingPage.js
+++ table1/client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,6 @@
+import "./LandingPage.css";
 import React, { useEffect } from "react";
 import axios from "axios";
-import "./LandingPage.css";
 function LandingPage() {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
@@ -9,12 +9,12 @@ function LandingPage() {
   return (
     <div className="main">
       <h3>환영합니다</h3>
-      <p>
+      <div className="loginMent">
         <a href="/login">로그인</a>을 하시겠습니까?
-      </p>
-      <p>
+      </div>
+      <div className="registerMent">
         계정이 없으시다면 <a href="/register">회원가입</a>도 가능합니다
-      </p>
+      </div>
     </div>
   );
 }
--- table1/client/src/components/views/LoginPage/LoginPage.js
+++ table1/client/src/components/views/LoginPage/LoginPage.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
 import "./LoginPage.css";
+import React, { useState } from "react";
+import Axios from "axios";
 function LoginPage() {
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
@@ -16,6 +17,18 @@ function LoginPage() {
     e.preventDefault();
     console.log("EMAIL: ", Email);
     console.log("PASSWORD: ", Password);
+    let body = {
+      email: Email,
+      password: Password,
+    };
+
+    console.log(body);
+    console.log(body.email);
+
+    Axios.post("/api/users/login", body).then((res) => {
+      console.log("SUCCESS");
+      console.log(res);
+    });
   };
 
   return (
