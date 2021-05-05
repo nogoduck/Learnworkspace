9cf9161bed6c903655060e107501dc6df2e1fc7e
adx <nogo22@naver.com>
2021-05-05 18:35:02 +0900

    인증 분기처리

--- table1/client/src/hoc/auth.js
+++ table1/client/src/hoc/auth.js
@@ -11,6 +11,20 @@ export default function (SpecificComponent, option, adminRoute = null) {
     useEffect(() => {
       dispatch(auth()).then((res) => {
         console.log(res);
+
+        if (!res.payload.isAuth) {
+          if (option) {
+            props.history.push("/login");
+          }
+        } else {
+          if (adminRoute && !res.payload.isAdmin) {
+            props.history.push("/");
+          } else {
+            if (option === false) {
+              props.history.push("/");
+            }
+          }
+        }
       });
     }, []);
 
