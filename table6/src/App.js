b25ac07d79f884f2895409a8b878257f5021a072
adx <nogo22@naver.com>
2021-05-10 15:48:21 +0900

    antd1

--- table6/src/App.js
+++ table6/src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import Main from "./views/Main";
+import Sub1 from "./views/Sub1";
+
 function App() {
-  return <>HI</>;
+  return (
+    <>
+      <BrowserRouter>
+        {/* <Sub1></Sub1> */}
+        <Route path="/" exact component={Main} />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ table6/src/views/Main.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Main.css";
+import { Row, Col, Input, Form, Button, Divider, Menu } from "antd";
+
+function Main() {
+  return (
+    <>
+      <div style={{ border: "5px solid yellow", width: "100%" }}>
+        <Row
+          gutter={[8, 8]}
+          style={{ border: "3px solid green", width: "100%" }}
+        >
+          {/* <div
+          style={{ width: "500px", height: "350px", border: "3px solid green" }}
+        ></div> */}
+          <Col span={12} style={{ border: "2px solid blue", width: "100%" }}>
+            1
+          </Col>
+          <Col span={12} style={{ border: "2px solid blue" }}>
+            2
+          </Col>
+        </Row>
+      </div>
+      <Menu>
+        <Menu.Item key="mail">
+          <Link to="/mail">MAIL</Link>
+        </Menu.Item>
+        <Menu.Item key="subscription">
+          <a href="/subscription">Subscription</a>
+        </Menu.Item>
+      </Menu>
+    </>
+  );
+}
+
+export default Main;
