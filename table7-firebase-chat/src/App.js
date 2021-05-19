2eb8d47bf4c4c4abdebd3372cfb220948dd47cbd
adx <nogo22@naver.com>
2021-05-20 07:23:11 +0900

    폴더 구조 최적화

--- table7-firebase-chat/src/App.js
+++ table7-firebase-chat/src/App.js
@@ -1,14 +1,19 @@
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import Main from "./views/Main";
-import Sub1 from "./views/Sub1";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+import ChatPage from "./components/ChatPage";
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        {/* <Sub1></Sub1> */}
-        <Route path="/" exact component={Main} />
-      </BrowserRouter>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ChatPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+        </Switch>
+      </Router>
     </>
   );
 }
