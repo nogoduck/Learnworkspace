abed1e3d8496f94e44b7701e576a74633936bdfe
adx <nogo22@naver.com>
2021-05-22 17:40:48 +0900

    Add Sidebar Design

--- table7-firebase-chat/src/components/ChatPage/SidePanel/ChatRoom.js
+++ table7-firebase-chat/src/components/ChatPage/SidePanel/ChatRoom.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import { MdGroup, MdAdd } from "react-icons/md";
 
-function ChatRoom() {
-  return <div>ChatRoom</div>;
+export class ChatRoom extends Component {
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            position: "relative",
+            display: "flex",
+            alignItems: "center",
+            // width: "100%",
+          }}
+        >
+          <MdGroup style={{ marginRight: "5" }} />
+          Chat Room (1)
+          <MdAdd
+            style={{
+              position: "absolute",
+              right: "0",
+              cursor: "pointer",
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ChatRoom;
