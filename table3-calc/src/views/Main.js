6d14fec889650a3b027c8a6713b6bcffeb09ca2e
adu <nogo22@naver.com>
2021-05-24 01:34:51 +0900

    css calc

--- table3-calc/src/views/Main.js
+++ table3-calc/src/views/Main.js
@@ -5,15 +5,41 @@ function Main() {
   return (
     <>
       <div id="calc__container">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
+        <input type="text"></input>
+
+        <div style={{ display: "flex" }}>
+          <div>
+            <div id="calc-top-func">
+              <button>n/a</button>
+              <button>.</button>
+              <button>+/-</button>
+              <button>C</button>
+              <button>()</button>
+              <button>%</button>
+            </div>
+            <div id="calc-number">
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>4</button>
+              <button>5</button>
+              <button>6</button>
+              <button>7</button>
+              <button>8</button>
+              <button>9</button>
+              <button style={{ width: "104px" }}>0</button>
+              <button>00</button>
+            </div>
+          </div>
+          <div id="calc-right-func">
+            <button>◀</button>
+            <button>/</button>
+            <button>*</button>
+            <button>-</button>
+            <button>+</button>
+            <button>=</button>
+          </div>
+        </div>
       </div>
     </>
   );
