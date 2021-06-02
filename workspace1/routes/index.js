371cf47bcc012c59fa454b024d1d720b0c623fb2
adx <nogo22@naver.com>
2021-06-02 20:08:55 +0900

    s

--- workspace1/routes/index.js
+++ workspace1/routes/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
-router.get("/", (req, res, next) => {
-  res.render("index");
+let filename = "";
+fs.readdir("./img/", function (error, filelist) {
+  filename = filelist;
+  console.log(filelist);
+});
+
+router.get("/", async (req, res, next) => {
+  console.log("filename: ", filename);
+  res.render("index", {
+    filename: filename,
+  });
 });
 
 module.exports = router;
