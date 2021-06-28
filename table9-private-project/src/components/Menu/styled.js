99a4605e90745ad241fffa2bf5ac61a7ac8d8ec0
adu <nogo22@naver.com>
2021-06-29 04:45:49 +0900

    table9 독립된 레포지토리로 분리전

--- table9-private-project/src/components/Menu/styled.js
+++ table9-private-project/src/components/Menu/styled.js
@@ -1,23 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  /* border: 2px solid black;
-  border-radius: 6px;
-  background-color: blue;
-  z-index: 1024;
-  user-select: none;
-  position: fixed; */
   position: fixed;
   top: 0;
   right: 0;
   left: 0;
   bottom: 0;
-  z-index: 1000;
+  z-index: 1024;
   & > div {
-    position: absolute;
     display: inline-block;
-    color: green;
-    border: 1px solid red;
+    position: absolute;
+    color: #000;
+    border: 2px solid black;
   }
 `;
 
@@ -27,7 +21,7 @@ export const CloseButton = styled.button`
   cursor: pointer;
   font-size: 32px;
   position: absolute;
-  top: 40px;
-  right: 2px;
-  color: yellow;
+  top: 2px;
+  right: 5px;
+  /* color: yellow; */
 `;
